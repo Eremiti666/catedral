684b35fa7d516e927abd21e14b5882d9b9ee1b17
Eremiti666 <luiedu.ere@gmail.com>
2026-07-21 00:48:59 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('Site carregado com sucesso!');
-});
+function mudarPagina(tipo) {
+  const pagPrincipal = document.getElementById('paginaPrincipal');
+  const pagLivro = document.getElementById('paginaLivro');
+  const btnPrincipal = document.getElementById('btnPrincipal');
+  const btnLivro = document.getElementById('btnLivro');
+
+  if (tipo === 'principal') {
+    pagPrincipal.classList.add('active');
+    pagLivro.classList.remove('active');
+    btnPrincipal.classList.add('active');
+    btnLivro.classList.remove('active');
+  } else if (tipo === 'livro') {
+    pagLivro.classList.add('active');
+    pagPrincipal.classList.remove('active');
+    btnLivro.classList.add('active');
+    btnPrincipal.classList.remove('active');
+  }
+}
